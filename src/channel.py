b2dbc67b2e7be2e8b1b91c25528c7ee8129e76ff
Ilya Ignatenko <ilya.ilya.ignatenko@gmail.com>
2023-07-19 23:05:30 +1000

    homework3 done!

--- src/channel.py
+++ src/channel.py
@@ -14,30 +14,50 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
         # self.api_key = os.getenv("YOU_API_KEY")
-        self.title = self.info()['items'][0]['snippet']['title']
-        self.description = self.info()['items'][0]['snippet']['description']
+        self.title = self.channel_date()['items'][0]['snippet']['title']
+        self.description = self.channel_date()['items'][0]['snippet']['description']
         self.url = 'https://www.youtube.com/channel/' + self.__channel_id
-        self.subscriber_count = self.info()['items'][0]['statistics']['subscriberCount']
-        self.video_count = self.info()['items'][0]['statistics']['videoCount']
-        self.view_count = self.info()['items'][0]['statistics']['viewCount']
+        self.subscriber_count = int(self.channel_date()['items'][0]['statistics']['subscriberCount'])
+        self.video_count = self.channel_date()['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel_date()['items'][0]['statistics']['viewCount']
+
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        return self.subscriber_count - other.subscriber_count
+
+    def __rsub__(self, other):
+        return other.subscriber_count - self.subscriber_count
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        return self.subscriber_count >= other.subscriber_count
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        return self.subscriber_count <= other.subscriber_count
+
+    def __eq__(self, other):
+        return self.subscriber_count == other.subscriber_count
 
     @property
     def channel_id(self):
         return self.__channel_id
 
-    def info(self):
+    def channel_date(self):
         """Возвращает в консоль информацию о канале."""
         youtube = build('youtube', 'v3', developerKey=self.api_key)
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
         return channel
-
-    def print_info(self):
-        """Выводит в консоль информацию о канале."""
-        youtube = build('youtube', 'v3', developerKey=self.api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-
-        return print(channel)
 
     @classmethod
     def get_service(cls):
